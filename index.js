72332d7b689bbfcc5fc3067ea3f3d960d436886a
github username <github email address>
2019-08-05 17:44:25 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// document.getElementById("main").remove();
+document.querySelector("main").remove()
+
+
+
+const newHeader = document.createElement("h1") //creating an H1 
+newHeader.id = "victory" // setting it's id to victory 
+newHeader.innerHTML = "David is the champion!"; // adding html text
+newHeader.className = "victory"; // setting new class name
