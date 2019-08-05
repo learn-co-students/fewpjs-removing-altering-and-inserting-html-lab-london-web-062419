d76c1f65ea6c1f19e449ce1aec24ea144450cc74
Aaron <aaron.ramharacksingh@gmail.com>
2019-08-05 20:17:34 +0100

    Done.

--- index.js
+++ index.js
@@ -7,4 +7,3 @@ document.querySelector("main#main").remove();
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
 newHeader.innerHTML = "Flatiron School is the champion!";
-newHeader.className = "victory";
